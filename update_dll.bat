577734f476f41dd9f7c887e6425fb127f25cc9a0
quentin-arzalier <quentin.arzalier@free.fr>
2024-05-22 09:57:23 +0200

    c'est bon pour le dll

--- update_dll.bat
+++ update_dll.bat
@@ -1,2 +1,2 @@
 mkdir Assets\AFM_DLL
-curl -o Assets\AFM_DLL\AFM_DLL.dll https://github.com/quentin-arzalier/Alchi-Fu-Mie_DLL/blob/main/AFM_DLL/bin/Debug/AFM_DLL.dll
+curl -L https://github.com/quentin-arzalier/Alchi-Fu-Mie_DLL/raw/main/AFM_DLL/bin/Debug/AFM_DLL.dll --output Assets\AFM_DLL\AFM_DLL.dll
--- update_dll.sh
+++ update_dll.sh
@@ -1,2 +1,2 @@
 mkdir Assets/AFM_DLL
-sudo curl -o Assets/AFM_DLL/AFM_DLL.dll https://github.com/quentin-arzalier/Alchi-Fu-Mie_DLL/blob/main/AFM_DLL/bin/Debug/AFM_DLL.dll
+sudo curl -L https://github.com/quentin-arzalier/Alchi-Fu-Mie_DLL/raw/main/AFM_DLL/bin/Debug/AFM_DLL.dll --output Assets\AFM_DLL\AFM_DLL.dll
